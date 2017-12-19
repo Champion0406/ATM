3d707c9c7bd1ee319c06b30edb68408fa2f36a34
champion <barney0406@gmail.com>
2017-12-19 11:46:09 +0800

    12/19

--- app/src/main/java/com/champion/atm/UserInfoActivity.java
+++ app/src/main/java/com/champion/atm/UserInfoActivity.java
@@ -3,16 +3,34 @@ package com.champion.atm;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 public class UserInfoActivity extends AppCompatActivity {
+    private static final String TAG = "";
     private EditText edNickname;
     private EditText edPhone;
+    private Spinner ages;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        ages = (Spinner) findViewById(R.id.ages);
+        ArrayList<String> data = new ArrayList<>();
+      //  for (int i = 15;i<=40;i++){
+      //      data.add(i+"");
+      //  }
+      //  String[] data = getResources().getStringArray(R.array.ages);
+        ArrayAdapter adapter = new ArrayAdapter(
+                this,android.R.layout.simple_list_item_1,data);
+        ages.setAdapter(adapter);
+
         edNickname = (EditText) findViewById(R.id.user_nickname);
         edPhone = (EditText) findViewById(R.id.user_phone);
         String edname = getSharedPreferences("info",MODE_PRIVATE)
@@ -23,6 +41,8 @@ public class UserInfoActivity extends AppCompatActivity {
         edPhone.setText(edphone);
         }
         public  void ok(View view){
+            Log.d(TAG,"ok:"+ages.getSelectedItem().toString());
+            int age = Integer.parseInt(ages.getSelectedItem().toString());
             String nickname = edNickname.getText().toString();
             String phone = edPhone.getText().toString();
             getIntent().putExtra("EXTRA_NICKNAME",nickname);
